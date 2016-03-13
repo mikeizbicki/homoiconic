76cc9ea4baf6890915a6c60d182c704fb3812b1d
Mike Izbicki <mike@izbicki.me>
2016-03-12 18:06:50 -0800

    cleaned up heterogeneous algebras and homomorphisms

--- Algebra.hs
+++ Algebra.hs
@@ -22,6 +22,113 @@ import GHC.Generics
 
 --------------------------------------------------------------------------------
 
+class (cxt a, cxt b) => Sub (cxt :: Type -> Constraint) a b where
+    embed :: Proxy cxt -> a -> b
+
+instance cxt a => Sub cxt a a where
+    embed _ = P.id
+
+instance Sub Semigroup Integer Rational where
+    embed _ = P.toRational
+
+class Pointed a where point :: a
+instance (Semigroup a, Semigroup b, Pointed b) => Sub Semigroup a (a,b) where
+    embed _ a = (a,point)
+
+instance (Semigroup a, Semigroup b, Pointed a) => Sub Semigroup b (a,b) where
+    embed _ b = (point,b)
+
+-- class a <: b where
+--     embed :: a -> b
+--
+-- instance a <: a where
+--     embed = P.id
+--
+-- instance Integer <: Rational where
+--     embed =
+--
+-- instance (a,b) <: a where
+--     embed = P.fst
+--
+-- instance (a,b) <: b where
+--     embed = P.snd
+--
+-- instance a <: Maybe a where
+--     embed = Just
+
+--------------------------------------------------------------------------------
+
+type (><) a b = Prod a b
+class (cxt1 a, cxt2 a) => Prod cxt1 cxt2 a
+instance (cxt1 a, cxt2 a) => Prod cxt1 cxt2 a
+
+class HetAlgebra (cxt :: Type -> Constraint) where
+    type SuperClasses (cxt :: Type -> Constraint) :: Type -> Constraint
+    type HetDomain cxt a :: *
+    type HetRange cxt a :: *
+    op :: cxt a => Proxy (cxt a) -> HetDomain cxt a -> HetRange cxt a
+
+instance HetAlgebra Semigroup where
+    type SuperClasses Semigroup = Topology
+    type HetDomain Semigroup a = (a,a)
+    type HetRange  Semigroup a = a
+    op _ (a1,a2) = a1+a2
+
+instance HetAlgebra Monoid where
+    type SuperClasses Monoid = Semigroup >< Topology
+    type HetDomain Monoid a = ()
+    type HetRange  Monoid a = a
+    op _ _ = zero
+
+instance HetAlgebra Topology where
+    type SuperClasses Topology = Topology
+    type HetDomain Topology a = (a,a)
+    type HetRange Topology a = Logic a
+    op _ (a1,a2) = a1==a2
+
+--------------------
+
+data Mor (cxt::Type->Constraint) a b where
+    Mor :: (cxt a, cxt b)
+        => (HetDomain cxt a -> HetDomain cxt b)
+        -> (HetRange  cxt a -> HetRange  cxt b)
+        -> Mor cxt a b
+
+class Invariant (a::Type) (name::Symbol) where
+    type InvariantDomain name a :: Type
+    type InvariantRange  name a :: Type
+    invariant :: a -> InvariantDomain name a -> Logic (InvariantRange name a)
+
+instance (HetAlgebra cxt, Topology (HetRange cxt b)) => Invariant (Mor cxt a b) "morphism" where
+    type InvariantDomain "morphism" (Mor cxt a b) = HetDomain cxt a
+    type InvariantRange  "morphism" (Mor cxt a b) = HetRange  cxt b
+    invariant (Mor f g) a = g (op (Proxy::Proxy (cxt a))    a)
+                         ==    op (Proxy::Proxy (cxt b)) (f a)
+
+--------------------
+
+data Box cxt where
+    Box :: forall cxt a. cxt -> Box cxt
+
+data Box2 (cxt :: Type -> Constraint) a b where
+    Box2 :: forall cxt x a b. cxt (x a b) => x a b -> Box2 cxt a b
+
+type (a+>b) cxt = Box2 cxt a b
+
+-- ($$) :: forall a b cxt. (a+>b) cxt -> a -> b
+-- ($$) (HomC f) = case (getMor f::Mor cxt a b) of Mor f g -> undefined
+
+data (a->>b) cxt where
+    HomC :: forall cxt cat a b. (Hom cxt cat, cxt a, cxt b) => cat a b -> (a->>b) cxt
+
+($$) :: forall a b cxt. (a->>b) cxt -> a -> b
+($$) (HomC f) = case (getMor f::Mor cxt a b) of Mor f g -> undefined
+
+class Hom (cxt :: Type -> Constraint) (cat :: Type -> Type -> Type) where
+    getMor :: (cxt a, cxt b) => cat a b -> Mor cxt a b
+
+--------------------------------------------------------------------------------
+
 class Topology a => Semigroup a where
     infixr 6 +
     (+) :: a -> a -> a
@@ -58,6 +165,9 @@ instance Semigroup Int where
 instance Semigroup Integer where
     (+) = (P.+)
 
+instance Semigroup Rational where
+    (+) = (P.+)
+
 instance Topology a => Semigroup [a] where
     (+) = (P.++)
 
@@ -71,15 +181,21 @@ instance Topology a => Semigroup [a] where
 class Semigroup a => Monoid a where
     zero :: a
 
-instance Lawful Monoid "idempotent" where
-    type LawInput Monoid "idempotent" a = a
+instance Lawful Monoid "idempotent_right" where
+    type LawInput Monoid "idempotent_right" a = a
     law _ _ _ a = a+zero == a
-               && zero+a == a
 
-instance Monoid Int     where zero = 0
-instance Monoid Integer where zero = 0
-instance Monoid Float   where zero = 0
-instance Monoid Double  where zero = 0
+instance Lawful Monoid "idempotent_left" where
+    type LawInput Monoid "idempotent_left" a = a
+    law _ _ _ a = zero+a == a
+
+instance Monoid Int      where zero = 0
+instance Monoid Integer  where zero = 0
+instance Monoid Float    where zero = 0
+instance Monoid Double   where zero = 0
+instance Monoid Rational where zero = 0
+
+----------------------------------------
 
 class Monoid a => Group a where
     {-# MINIMAL negate | (-) #-}
@@ -88,6 +204,26 @@ class Monoid a => Group a where
 
     (-) :: a -> a -> a
     a1-a2 = a1 + negate a2
+
+instance Lawful Group "negate" where
+    type LawInput Group "negate" a = a
+    law _ _ _ a = negate a == zero - a
+
+instance Lawful Group "(-)" where
+    type LawInput Group "(-)" a = (a,a)
+    law _ _ _ (a1,a2) = a1-a2 == a1+negate a2
+
+instance Lawful Group "cancellative" where
+    type LawInput Group "cancellative" a = a
+    law _ _ _ a = zero == a-a
+
+instance Group Int          where negate = P.negate
+instance Group Integer      where negate = P.negate
+instance Group Float        where negate = P.negate
+instance Group Double       where negate = P.negate
+instance Group Rational     where negate = P.negate
+
+----------------------------------------
 
 class Group a => Ring a where
     one :: a
