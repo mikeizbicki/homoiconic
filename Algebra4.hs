a926451061cccdadff0d4c59181b7b36a4548b6f
Mike Izbicki <mike@izbicki.me>
2016-04-05 18:30:09 -0700

    class hierarchies work

--- Algebra4.hs
+++ Algebra4.hs
@@ -29,7 +29,8 @@ class Semigroup a where
 
 type ValidScalar a = (Scalar a~a, Module a)
 
-class (ValidScalar (Scalar a), Semigroup a) => Module a where
+-- class (ValidScalar (Scalar a), Semigroup a) => Module a where
+class Semigroup a => Module a where
     (.*) :: a -> Scalar a -> a
 
 class Module a => Hilbert a where
@@ -37,22 +38,93 @@ class Module a => Hilbert a where
 
 --------------------------------------------------------------------------------
 
-instance (Show (AppAT t a), Num (AppAT t a)) => Num (E t a) where fromInteger = Ei . fromInteger
-
--- type instance Scalar (E t a) = E (TagAT 'Scalar t) a
--- type instance Scalar (E t a) = E (AppAT 'Scalar t) a
-type instance Scalar (E t a) = E (NestAT 'Scalar t) a
--- type instance Scalar (E t a) = E t (Scalar a)
-
---------------------
-
-unTag :: E (TagAT t1 t2) a -> E (NestAT t1 t2) a
+unTag :: e (TagAT t1 t2) a -> e (NestAT t1 t2) a
 unTag = unsafeCoerce
 
-reTag :: E (NestAT t1 t2) a -> E (TagAT t1 t2) a
+reTag :: e (NestAT t1 t2) a -> e (TagAT t1 t2) a
 reTag = unsafeCoerce
 
+--------------------------------------------------------------------------------
+
+instance (Show (AppAT t a), Num (AppAT t a)) => Num (F Semigroup t a)
+    where fromInteger = Fi . fromInteger
+instance (Show (AppAT t a), Num (AppAT t a)) => Num (F Module t a)
+    where fromInteger = FM . fromInteger
+instance (Show (AppAT t a), Num (AppAT t a)) => Num (F Hilbert t a)
+    where fromInteger = FH . fromInteger
+
+type instance Scalar (F cxt t a) = F cxt (NestAT 'Scalar t) a
+
+class FAlgebra (cxt::Type->Constraint) where
+    data F cxt (t::AT) a
+    runF :: F cxt t a -> AppAT t a
+    mkF :: Show (AppAT t a) => AppAT t a -> F cxt t a
+
 --------------------
+
+instance FAlgebra Semigroup where
+    data F Semigroup t a where
+        Fi :: Show (AppAT t a) => AppAT t a -> F Semigroup t a
+        Fp ::
+            ( Show (F cxt t a)
+            , FAlgebra cxt
+            , Semigroup (AppAT t a)
+            ) => F cxt t a -> F cxt t a -> F Semigroup t a
+    runF (Fi a) = a
+    runF (Fp a1 a2) = runF a1+runF a2
+    mkF = Fi
+
+instance (Semigroup (AppAT t a)) => Semigroup (F Semigroup t a) where (+) e1 e2 = Fp e1 e2
+
+instance Show (F Semigroup t a) where
+    show (Fi a) = show a
+    show (Fp a1 a2) = "("++show a1++"+"++show a2++")"
+
+--------------------
+
+instance FAlgebra Module where
+    data F Module t a where
+        FM :: {-#UNPACK#-}!(F Semigroup t a) -> F Module t a
+        Fm ::
+            ( FAlgebra cxt
+            , Show (F cxt t a)
+            , Show (F cxt ('TagAT 'Scalar t) a)
+            , Module (AppAT t a)
+            ) => F cxt t a -> F cxt (TagAT 'Scalar t) a -> F Module t a
+    runF (FM m) = runF m
+    runF (Fm a1 a2) = runF a1.*runF a2
+    mkF = FM . mkF
+
+instance (Semigroup (AppAT t a)) => Semigroup (F Module t a) where (+ ) a1 a2 = FM $ Fp a1 a2
+instance (Module    (AppAT t a)) => Module    (F Module t a) where (.*) a1 a2 =      Fm a1 (reTag a2)
+
+instance Show (F Module t a) where
+    show (FM a) = show a
+    show (Fm a1 a2) = "("++show a1++".*"++show a2++")"
+
+--------------------
+
+instance FAlgebra Hilbert where
+    data F Hilbert t a where
+        FH :: {-#UNPACK#-}!(F Module t a) -> F Hilbert t a
+        Fd :: Hilbert (AppAT t a) => F Hilbert t a -> F Hilbert t a -> F Hilbert (TagAT 'Scalar t) a
+    runF (FH h) = runF h
+    runF (Fd a1 a2) = runF a1<>runF a2
+    mkF = FH . mkF
+
+instance (Semigroup (AppAT t a)) => Semigroup (F Hilbert t a) where (+ ) a1 a2 = FH $ FM $ Fp a1 a2
+instance (Module    (AppAT t a)) => Module    (F Hilbert t a) where (.*) a1 a2 = FH $      Fm a1 (reTag a2)
+instance (Hilbert   (AppAT t a)) => Hilbert   (F Hilbert t a) where (<>) a1 a2 = unTag $   Fd a1 a2
+
+instance Show (F Hilbert t a) where
+    show (FH a) = show a
+    show (Fd a1 a2) = "("++show a1++"<>"++show a2++")"
+
+--------------------------------------------------------------------------------
+
+instance (Show (AppAT t a), Num (AppAT t a)) => Num (E t a) where fromInteger = Ei . fromInteger
+
+type instance Scalar (E t a) = E (NestAT 'Scalar t) a
 
 data E (t::AT) a where
     Ei :: Show      (AppAT t a) => AppAT t a                      -> E t a
@@ -105,17 +177,9 @@ type instance AppAT 'Id a = a
 type instance AppAT 'Scalar a = Scalar a
 type instance AppAT (TagAT t t') a = AppAT t (AppAT t' a)
 
-type family NestAT (t::AT) (a::AT) :: AT where
-    NestAT t       'Id     = t
---     NestAT t1      t2      = TagAT t1 t2
-    NestAT 'Scalar 'Scalar = 'Scalar
-
---------------------------------------------------------------------------------
-
--- class FAlgebra (cxt::Type->Constraint) a where
---     data F cxt t a
---
--- instance FAlgebra
+type family NestAT (t::AT) (a::AT) :: AT
+type instance NestAT t       'Id     = t
+type instance NestAT 'Scalar 'Scalar = 'Scalar
 
 --------------------------------------------------------------------------------
 
@@ -129,73 +193,11 @@ instance Semigroup Int where (+) = (P.+)
 instance Module Int where (.*) = (P.*)
 instance Hilbert Int where (<>) = (P.*)
 
--- type instance Scalar (a,b) = Scalar b
--- instance (Semigroup a,Semigroup b) => Semigroup (a,b) where
---     (a1,b1)+(a2,b2) =(a1+a2,b1+b2)
--- instance (Module a, Module b, Scalar a~Scalar b) => Module (a,b) where
---     (a1,b1).*r =(a1.*r,b1.*r)
--- instance (Hilbert a, Hilbert b, Scalar a~Scalar b, ValidScalar b) => Hilbert (a,b) where
---     (a1,b1)<>(a2,b2) =(a1<>a2)+(b1<>b2)
+type instance Scalar (a,b) = Scalar b
+instance (Semigroup a,Semigroup b) => Semigroup (a,b) where
+    (a1,b1)+(a2,b2) =(a1+a2,b1+b2)
+instance (Module a, Module b, Scalar a~Scalar b) => Module (a,b) where
+    (a1,b1).*r =(a1.*r,b1.*r)
+instance (Hilbert a, Hilbert b, Scalar a~Scalar b, ValidScalar b) => Hilbert (a,b) where
+    (a1,b1)<>(a2,b2) =(a1<>a2)+(b1<>b2)
 
-----------------------------------------
-
-data Expr (t::AT) a where
-    Expr_plus :: Semigroup (AppAT t a)  => Expr t a -> Expr t a                     -> Expr t a
---     Expr_mul  ::
---         ( Module (AppAT t a)
---         ) => Expr t a -> Expr (TagAT 'Scalar t) a     -> Expr t a
-    Expr_mul  :: Module (AppAT t a)     => Expr t a -> Expr (TagAT 'Scalar t) a     -> Expr t a
---     Expr_mul  :: Module (AppAT t a)     => Expr t a -> Expr t (Scalar a)            -> Expr t a
---     Expr_dp   :: Hilbert (AppAT t a)    => Expr t a -> Expr t a                     -> Expr (TagAT 'Scalar t) a
-
-    Expr_Id   :: AppAT t a          -> Expr t a
-
-mkExpr = Expr_Id
-
--- type instance Scalar (Expr t a) = Expr (AppAT 'Scalar t) a
--- -- type instance Scalar (Expr t a) = Expr t (Scalar a)
-
---------------------
-
-instance Semigroup (AppAT t a) => Semigroup (Expr t a) where
-    (+) = Expr_plus
-
--- instance (ValidScalar (Expr t a), Module (AppAT t a)) => Module (Expr t a) where
---     (.*) = Expr_mul
-
--- instance (ValidScalar (AppAT t a), Hilbert (AppAT t a)) => Hilbert (Expr t a) where
---     (<>) = Expr_dp
-
-----------------------------------------
-
--- go :: Expr t a -> AppAT t a
--- go (Expr_Id a) = a
--- go (Expr_plus e1 e2) = go e1+go e2
--- go (Expr_mul  e1 e2) = go e1.*go e2
--- go (Expr_dp   e1 e2) = go e1<>go e2
-
-----------------------------------------
-
-instance
-    ( Show (AppAT t a)
-    , Show (Scalar (AppAT t a))
-    , Scalar (Scalar (AppAT t a))~Scalar (AppAT t a)
-    ) => Show (Expr t a)
-        where
-    show (Expr_Id a)       = show a
-    show (Expr_plus e1 e2) = "("++show e1++"+" ++show e2++")"
-    show (Expr_mul  e1 e2) = "("++show e1++".*"++show e2++")"
-
-instance {-#overlaps#-}
-    ( Show (Scalar (AppAT t a))
-    , Show (AppAT t a)
-    , Scalar (Scalar (AppAT t a))~Scalar (AppAT t a)
-    ) => Show (Expr (TagAT 'Scalar t) a)
-        where
-    show (Expr_Id a)       = show a
-    show (Expr_plus e1 e2) = "("++show e1++"+" ++show e2++")"
-    show (Expr_mul  e1 e2) = "("++show e1++".*"++show e2++")"
---     show (Expr_dp   e1 e2) = "("++show e1++"<>"++show e2++")"
-
-instance Num (AppAT t a) => Num (Expr t a) where
-    fromInteger = Expr_Id . fromInteger
