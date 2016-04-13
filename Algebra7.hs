e3490589668599b592621274499b04b43c881fa7
Mike Izbicki <mike@izbicki.me>
2016-04-12 22:09:32 -0700

    I think the Taggable class solves the problem of class constraints at least for the Scalar case; there's probably more exotic cases it doesn't cover though

--- Algebra7.hs
+++ Algebra7.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE UndecidableSuperClasses #-}
+{-# LANGUAGE TypeFamilyDependencies #-}
+-- {-# LANGUAGE AllowAmbiguousTypes #-}
 module Algebra7
     where
 
@@ -16,7 +19,9 @@ class Topology a where
 class Topology a => Semigroup a where
     (+) :: a -> a -> a
 
-class Semigroup a => Module a where
+-- class (Scalar (Scalar a)~Scalar a, Semigroup a) => Module a where
+-- class Semigroup a => Module a where
+class (Semigroup a, Module (Scalar a)) => Module a where
     type family Scalar a
     (.*) :: Scalar a -> a -> a
 
@@ -51,20 +56,21 @@ instance (Hilbert a, Hilbert b, Semigroup (Scalar b), Scalar a~Scalar b) => Hilb
 
 type Space = Hilbert
 
-x :: Expr Space 'Id (Int,Int)
+x :: Expr Space (Tag 'Id 'Id) (Int,Int)
 x = Pure (2,2)
 
-y :: Expr Space 'Id (Int,Int)
+y :: Expr Space (Tag 'Id 'Id) (Int,Int)
 y = Pure (1,3)
 
-z :: Expr Space (Tag 'Scalar 'Id) (Int,Int)
+z :: Scalar (Expr Space (Tag 'Id 'Id) (Int,Int))
 z = Pure 1
 
 --------------------------------------------------------------------------------
 
 data Free (f::AT->Type->Type) (t::AT) (a::Type) where
-    FreeTag  :: f (Tag s t) (Free f t a)  -> Free f (Tag s t) a
-    Free     :: f        t  (Free f t a)  -> Free f        t  a
+--     FreeTag  :: Proxy s ->      f (MaybeTag s t  ) (Free f t a)  -> Free f (MaybeTag s t  ) a
+    FreeTag  :: Proxy s -> Proxy t -> f (     Tag s t  ) (Free f t a)  -> Free f (     Tag s t  ) a
+    Free     ::                       f             t    (Free f t a)  -> Free f             t    a
     Pure     :: App t a -> Free f t a
 
 instance
@@ -73,12 +79,22 @@ instance
     , ShowUntag (f t (Free f t a))
     ) => Show (Free f t a)
         where
-    show (FreeTag f) = show f
-    show (Free    f) = show f
-    show (Pure    a) = show a
+    show (FreeTag _ _ f) = show f
+    show (Free        f) = show f
+    show (Pure        a) = show a
+
+-- instance {-#OVERLAPS#-}
+--     ( Show      (App (Tag 'Scalar t) a)
+-- --     , Show (f (Tag 'Scalar t) (Free f t a))
+--     ) => Show (Free f (Tag 'Scalar t) a)
+--         where
+-- --     show (FreeTag _ f) = show f
+--     show (Pure      a) = show a
+
 
 type family ShowUntag (f::Type) :: Constraint where
-    ShowUntag (f (Tag s t) (Free f (Tag s t) a))  = Show (f (Tag s t) (Free f t a))
+--     ShowUntag (f (Tag 'Id t) (Free f (Tag 'Id t) a))  = Show (f (Tag 'Id t) (Free f (Tag 'Id t) a))
+    ShowUntag (f (Tag s   t) (Free f (Tag s   t) a))  = Show (f (Tag s   t) (Free f          t  a))
     ShowUntag _ = ()
 
 instance Functor (f t) => Functor (Free f t) where
@@ -104,15 +120,15 @@ class FAlgebra (alg::Type->Constraint) where
 
 instance FAlgebra Topology where
     data Sig Topology t a where
-        Se :: a -> a -> Sig Topology (Tag 'Logic t) a
+        Se :: a -> a -> Sig Topology (MaybeTag 'Logic t) a
 --     alg (Se a1 a2) = a1==a2
 
 instance Show a => Show (Sig Topology t a) where
     show (Se a1 a2) = "("++show a1++"=="++show a2++")"
 
 instance Topology (Expr Topology t a) where
-    type Logic (Expr Topology t a) = Expr Topology (Tag 'Logic t) a
-    (==) e1 e2 = FreeTag $ Se e1 e2
+    type Logic (Expr Topology t a) = Expr Topology (MaybeTag 'Logic t) a
+    (==) e1 e2 = FreeTag (Proxy::Proxy 'Logic) (Proxy::Proxy t) $ Se e1 e2
 
 ----------------------------------------
 
@@ -131,8 +147,8 @@ instance Show a => Show (Sig Semigroup t a) where
 --     fmap f (Sp a1 a2) = Sp (f a1) (f a2)
 
 instance Topology (Expr Semigroup t a) where
-    type Logic (Expr Semigroup t a) = Expr Semigroup (Tag 'Logic t) a
-    (==) e1 e2 = FreeTag $ SS $ Se e1 e2
+    type Logic (Expr Semigroup t a) = Expr Semigroup (MaybeTag 'Logic t) a
+    (==) e1 e2 = FreeTag (Proxy::Proxy 'Logic) (Proxy::Proxy t) $ SS $ Se e1 e2
 
 instance Semigroup (Expr Semigroup t a) where
     (+) e1 e2 = Free $ Sp e1 e2
@@ -168,14 +184,14 @@ instance {-#OVERLAPS#-}
     show (SM m) = show m
 
 instance Topology (Expr Module t a) where
-    type Logic (Expr Module t a) = Expr Module (Tag 'Logic t) a
-    (==) e1 e2 = FreeTag $ SM $ SS $ Se e1 e2
+    type Logic (Expr Module t a) = Expr Module (MaybeTag 'Logic t) a
+    (==) e1 e2 = FreeTag (Proxy::Proxy 'Logic) (Proxy::Proxy t) $ SM $ SS $ Se e1 e2
 
 instance Semigroup (Expr Module t a) where
     (+) e1 e2 = Free $ SM $ Sp e1 e2
 
 instance Module (Expr Module t a) where
-    type Scalar (Expr Module t a) = Expr Module (Tag 'Scalar t) a
+    type Scalar (Expr Module t a) = Expr Module (MaybeTag 'Scalar t) a
     (.*) e1 e2 = Free $ Sm e1 e2
 
 -- NOTE:
@@ -191,9 +207,11 @@ instance Module (Expr Module t a) where
 instance FAlgebra Hilbert where
     data Sig Hilbert t a where
         SH :: Sig Module t a -> Sig Hilbert t a
-        Sd :: a
+        Sd :: Proxy t
            -> a
-           -> Sig Hilbert (Tag 'Scalar t) a
+           -> a
+           -> Sig Hilbert (MaybeTag 'Scalar t) a
+--            -> Sig Hilbert (Tag 'Scalar t) a
 
     alg (SH h) = alg h
 
@@ -203,14 +221,14 @@ instance
     ) => Show (Sig Hilbert t a)
         where
     show (SH h) = show h
-    show (Sd a1 a2) = "("++show a1++"<>"++show a2++")"
+    show (Sd _ a1 a2) = "("++show a1++"<>"++show a2++")"
 
 instance {-#OVERLAPS#-}
     ( Show a
     ) => Show (Sig Hilbert (Tag 'Scalar (Tag 'Scalar t)) a)
         where
     show (SH h) = show h
-    show (Sd a1 a2) = "("++show a1++"<>"++show a2++")"
+    show (Sd _ a1 a2) = "("++show a1++"<>"++show a2++")"
 
 instance {-#OVERLAPS#-}
     ( Show a
@@ -222,18 +240,32 @@ instance {-#OVERLAPS#-}
 --     fmap f (SH h) = SH $ fmap f h
 
 instance Topology (Expr Hilbert t a) where
-    type Logic (Expr Hilbert t a) = Expr Hilbert (Tag 'Logic t) a
-    (==) e1 e2 = FreeTag $ SH $ SM $ SS $ Se e1 e2
+    type Logic (Expr Hilbert t a) = Expr Hilbert (MaybeTag 'Logic t) a
+    (==) e1 e2 = FreeTag (Proxy::Proxy 'Logic) (Proxy::Proxy t) $ SH $ SM $ SS $ Se e1 e2
 
 instance Semigroup (Expr Hilbert t a) where
     (+) e1 e2 = Free $ SH $ SM $ Sp e1 e2
 
 instance Module (Expr Hilbert t a) where
-    type Scalar (Expr Hilbert t a) = Expr Hilbert (Tag 'Scalar t) a
+    type Scalar (Expr Hilbert t a) = Expr Hilbert (MaybeTag 'Scalar t) a
     (.*) e1 e2 = Free $ SH $ Sm e1 e2
 
-instance Hilbert (Expr Hilbert t a) where
-    (<>) e1 e2 = FreeTag $ Sd e1 e2
+instance Taggable 'Scalar t => Hilbert (Expr Hilbert t a) where
+    (<>) e1 e2 = freeTag
+        (Proxy::Proxy 'Scalar)
+        (Proxy::Proxy t)
+        $ Sd (Proxy::Proxy t) e1 e2
+
+--------------------
+
+class Taggable (s::AT) (t::AT) where
+    freeTag  :: Proxy s -> Proxy t -> f (MaybeTag s t  ) (Free f t a)  -> Free f (MaybeTag s t  ) a
+
+instance {-#OVERLAPS#-} Taggable 'Scalar (Tag 'Scalar t) where
+    freeTag _ _ = Free
+
+instance {-#OVERLAPS#-} MaybeTag r (Tag s t) ~ Tag r (Tag s t) => Taggable r (Tag s t) where
+    freeTag p1 p2 = FreeTag p1 p2
 
 --------------------------------------------------------------------------------
 
@@ -248,6 +280,11 @@ type instance App 'Id a = a
 type instance App 'Scalar a = Scalar a
 type instance App 'Logic  a = Logic  a
 type instance App (Tag t t') a = App t (App t' a)
+
+-- type family MaybeTag (s::AT) (t::AT) = r | r -> s t where
+type family MaybeTag (s::AT) (t::AT) where
+    MaybeTag 'Scalar (Tag 'Scalar t) = (Tag 'Scalar t)
+    MaybeTag s t = Tag s t
 
 type family Nest (t::AT) (a::AT) :: AT where
     Nest 'Id     t       = t
